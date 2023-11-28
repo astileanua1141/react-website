b53f5bc73ffb3f0eb4eaadffe271e9cf388c6a29
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2023-11-28 20:08:12 +0200

    delegate handle button clicks to an outside component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
+  let tabContent = 'Please select a button';
+
+  function handleSelect(selectedButton) {
+    //selectedButton => 'components', 'jsx', 'props', 'state'
+    console.log('Selected btn: ' + selectedButton);
+    // tabContent = selectedButton;
+  }
+
   return (
     <div>
       <Header />
@@ -19,6 +28,18 @@ function App() {
               image={CORE_CONCEPTS[3].image}
             />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {tabContent}
         </section>
       </main>
     </div>
