d75f320733165f1497de64ff3f711afe676997bd
Alexandra Astileanu <alexandra.astileanu@gmail.com>
2023-11-28 21:42:50 +0200

    Use react hooks to manage state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { CORE_CONCEPTS } from './data.js';
+import { useState } from 'react';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  let tabContent = 'Please select a button';
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
     //selectedButton => 'components', 'jsx', 'props', 'state'
     console.log('Selected btn: ' + selectedButton);
-    // tabContent = selectedButton;
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = 'Please select a topic';
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
